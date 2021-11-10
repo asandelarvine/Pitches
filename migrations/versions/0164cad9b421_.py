1872f94bbf55dd1ba281430a359541bb9f17f7a2
asandelarvine <larvine.asande@student.moringaschool.com>
2021-11-10 07:52:23 +0300

    migrations added

---
+++ migrations/versions/0164cad9b421_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 0164cad9b421
+Revises: 0139370e433c
+Create Date: 2021-11-10 00:52:35.495276
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0164cad9b421'
+down_revision = '0139370e433c'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
