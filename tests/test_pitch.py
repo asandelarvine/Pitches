e5492699f9913389a8609fbc46d0e59d0e9fcf24
asandelarvine <larvine.asande@student.moringaschool.com>
2021-11-10 14:16:14 +0300

    method set before every test is run

--- tests/test_pitch.py
+++ tests/test_pitch.py
@@ -4,14 +4,14 @@ Pitch = Pitch
 
 class PitchTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the Pitch class
+    Test Class to test the behavior of the Pitch class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_pitch = Pitch(133,'killed by excelence')
+        self.new_pitch = Pitch(133,'killed')
 
     def test_instance(self):
         self.assertTrue(isinstance(self.new_pitch,Pitch))
