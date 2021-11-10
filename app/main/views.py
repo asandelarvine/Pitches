7178ac7db4cff474566c0d50a078ee10b6cf6dc0
asandelarvine <larvine.asande@student.moringaschool.com>
2021-11-10 14:43:46 +0300

    final commits

--- app/main/views.py
+++ app/main/views.py
@@ -32,16 +32,16 @@ def interview():
     title = 'Home - Welcome to The best Pitching Website Online'  
     return render_template('interview.html', title = title, pitches= pitches )
 
-@main.route('/pick_up_lines/pitches/')
-def pick_up_line():
+@main.route('/elevator/pitches/')
+def elevator():
     '''
     View root page function that returns the index page and its data
     '''
-    title = 'Pick Up Lines'
+    title = 'Elevator Pitches'
 
     pitches= Pitch.get_all_pitches()
 
-    return render_template('pick_up_lines.html', title = title, pitches= pitches )
+    return render_template('elevator.html', title = title, pitches= pitches )
 
 @main.route('/promotion/pitches/')
 def promotion():
